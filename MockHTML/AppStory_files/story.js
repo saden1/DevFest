7a4d9dc0d0c38eb964ab5289758ce2ef025aed60
Sharmarke Aden <saden1@gmail.com>
2012-09-29 15:56:58 -0700

    updated mock ui

---
+++ MockHTML/AppStory_files/story.js
@@ -0,0 +1,224 @@
+var story = {
+ "title": "AppStory",
+ "pages": [
+  {
+   "title": "HomeListView",
+   "height": 844,
+   "width": 430,
+   "image": "HomeListView.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      37,
+      658,
+      209,
+      706
+     ]
+    },
+    {
+     "page": 1,
+     "rect": [
+      209,
+      658,
+      397,
+      706
+     ]
+    }
+   ]
+  },
+  {
+   "title": "HomeMapView",
+   "height": 844,
+   "width": 430,
+   "image": "HomeMapView.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      37,
+      658,
+      209,
+      706
+     ]
+    },
+    {
+     "page": 1,
+     "rect": [
+      209,
+      658,
+      397,
+      706
+     ]
+    }
+   ]
+  },
+  {
+   "title": "PlaceDetails",
+   "height": 844,
+   "width": 430,
+   "image": "PlaceDetails.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      40,
+      146,
+      49,
+      170
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      357,
+      146,
+      381,
+      170
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      357,
+      146,
+      381,
+      170
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      37,
+      182,
+      397,
+      207
+     ]
+    }
+   ]
+  },
+  {
+   "title": "PlaceAction",
+   "height": 844,
+   "width": 430,
+   "image": "PlaceAction.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      40,
+      146,
+      49,
+      170
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      357,
+      146,
+      381,
+      170
+     ]
+    }
+   ]
+  },
+  {
+   "title": "EventDetails",
+   "height": 844,
+   "width": 430,
+   "image": "EventDetails.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      40,
+      146,
+      49,
+      170
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      357,
+      146,
+      381,
+      170
+     ]
+    }
+   ]
+  },
+  {
+   "title": "EventAction",
+   "height": 844,
+   "width": 430,
+   "image": "EventAction.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      40,
+      146,
+      49,
+      170
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      357,
+      146,
+      381,
+      170
+     ]
+    }
+   ]
+  },
+  {
+   "title": "PlaceDetails",
+   "height": 844,
+   "width": 430,
+   "image": "PlaceDetails.png",
+   "links": [
+    {
+     "page": 0,
+     "rect": [
+      40,
+      146,
+      49,
+      170
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      357,
+      146,
+      381,
+      170
+     ]
+    },
+    {
+     "page": 3,
+     "rect": [
+      357,
+      146,
+      381,
+      170
+     ]
+    },
+    {
+     "page": 2,
+     "rect": [
+      37,
+      182,
+      397,
+      207
+     ]
+    }
+   ]
+  }
+ ],
+ "highlightLinks": true
+}
---
+++ MockUI/AppStory.story
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<story:Storyboard xmlns:story="http://wireframesketcher.com/1.0/model/story">
+  <panels x="75" y="75">
+    <screen href="HomeListView.screen#/"/>
+  </panels>
+  <panels x="375" y="75">
+    <screen href="HomeMapView.screen#/"/>
+  </panels>
+  <panels x="675" y="75">
+    <screen href="place/PlaceDetails.screen#/"/>
+  </panels>
+  <panels x="75" y="375">
+    <screen href="place/actions/PlaceAction.screen#/"/>
+  </panels>
+  <panels x="975" y="75">
+    <screen href="place/EventDetails.screen#/"/>
+  </panels>
+  <panels x="675" y="375">
+    <screen href="place/actions/EventAction.screen#/"/>
+  </panels>
+  <panels x="375" y="375">
+    <screen href="place/PlaceDetails.screen#/"/>
+  </panels>
+</story:Storyboard>
